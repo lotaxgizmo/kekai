dfa1e261a9d868b40f9e0ae4a0f2c859bd959dda
lotaxgizmo <lotaoko2@gmail.com>
2024-04-17 16:07:28 +0100

    dds

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Buykekai from "../assets/buykekai.png"
 
 function Header() {
   return (
-    <div className=' w-full flex flero  justify-between items-center px-8 absolute z-30 mt-4'>
+    <div className=' w-full flex flero  justify-between items-center px-8 fixed top-0 z-30 pt-1 bg-black/20 '>
       <img src={sitelogo} alt="" className='w-32 lg:w-auto'/>
       {/* menupart */}
       <img src={Buykekai} alt="" />
